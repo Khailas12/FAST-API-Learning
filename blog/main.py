b753cd6b8032dae4877ea9b103f73386dffe55cb
Khailas12 <khailas303@gmail.com>
2024-11-23 03:46:34 +0530

    User role based CRUD along with passlib pwd hashing added

--- blog/main.py
+++ blog/main.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from typing import List
 from fastapi import Depends, FastAPI, status, HTTPException
 from .database import engine, SessionLocal
-from . import schemas, models
+from . import schemas, models, hashing
 from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
@@ -15,9 +16,9 @@ def get_db():
         yield db
     finally:
         db.close()
+        
 
-
-@app.post('/blog', status_code=status.HTTP_201_CREATED, response_model=schemas.Blog)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, response_model=schemas.Blog, tags=['blog'])
 def create(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
@@ -25,12 +26,12 @@ def create(blog: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog', response_model=List[schemas.Blog])
+@app.get('/blog', response_model=List[schemas.Blog], tags=['blog'])
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_201_CREATED, response_model=schemas.Blog)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.Blog, tags=['blog'])
 def get_entity(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -39,7 +40,7 @@ def get_entity(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
     return blog
 
-@app.put('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.put('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.Blog, tags=['blog'])
 def update_content(id: int, blog_data: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id)
     
@@ -58,10 +59,69 @@ def update_content(id: int, blog_data: schemas.Blog, db: Session = Depends(get_d
     return blog_first
 
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blog'])
 def delete_content(id: int, db: Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
     db.commit()
     return "Delete Successful"
 
 
+# User
+@app.post('/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser, tags=['user'])
+def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(models.User.name==user.name or models.User.email==user.email).first()
+    
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Name or Email already registered"
+        )
+    
+    pwd_hashing = hashing.Hasher.get_password_hash(user.password)
+    new_user = models.User(name=user.name, email=user.email, password=pwd_hashing)
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/user', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowUser], tags=['user'])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
+@app.get('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser, tags=['user'])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        detail = {"Error": f"user with {id} does not exist"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+    return user
+
+@app.put('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser, tags=['user'])
+def update_user(id: int, user: schemas.ShowUser, db: Session = Depends(get_db)):
+    user_data = db.query(models.User).filter(models.User.id==id)
+    
+    user_first = user_data.first()
+    if not user_data:
+        detail = {"Error": f"user with {id} does not exist"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+    
+    blog_data_dict = user.model_dump()  
+    blog_data_dict["updated_at"] = datetime.now() 
+    
+    user_data.update(blog_data_dict, synchronize_session=False)
+    
+    db.commit()
+    db.refresh(user_first)
+    return user_first
+
+
+@app.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['user'])
+def delete_content(id: int, db: Session = Depends(get_db)):
+    db.query(models.User).filter(models.User.id==id).delete(synchronize_session=False)
+    db.commit()
+    return "Delete Successful"
--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,19 @@ from sqlalchemy.sql import func
 
 class Blog(Base):
     __tablename__ = "blogs"
-    
+     
     id = Column(Integer, primary_key=True)
     title = Column(String)
     body = Column(String)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=None)
+    
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=None)
